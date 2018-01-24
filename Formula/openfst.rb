e13bf43e792dddc319065e4cc6eb7de077b86e66
Vincent Trumpff <vincent-tr@users.noreply.github.com>
2018-01-24 10:31:19 +0100

    Update openfst.rb

--- Formula/openfst.rb
+++ Formula/openfst.rb
@@ -5,13 +5,11 @@ class Openfst < Formula
   sha256 "5c28b6ccd017fc6ff94ebd0c73ed8ab37d48f563dab1c603856fb05bc9333d99"
 
   bottle do
+    root_url "http://homebrew.stoic.com"
     cellar :any
-    sha256 "62f64bd93abe2834f07e1ee79a363d1cfa106f4e9faaad8e1f3537f772e88b1e" => :sierra
-    sha256 "695595240d43745060ecafa520178c8890dea8b34f47fb5f174f85bcddad9f63" => :el_capitan
-    sha256 "97b714421950df1d9693c294e5d8f69ca167474737e43fe6b1305f5269a98b54" => :yosemite
-    sha256 "b20ee657fe8fdd2c8bfde124e1aff050b0724eb061b2bb820a2d928299f24e6e" => :x86_64_linux
+    sha256 "d5c9dca51d5e3a2fdfde26cdf8eaa2e7ae16da23b0982acc4e712247c7491011" => :high_sierra
   end
-
+  
   needs :cxx11
 
   depends_on "zlib" unless OS.mac?
