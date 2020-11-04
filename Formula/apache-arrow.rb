8b3706a34f24da6433012b542ee9e92a94a5854f
Yves <yves@sutoiku.com>
2020-11-04 14:37:14 -0500

    Upgrade to Arrow 2.0

--- Formula/apache-arrow.rb
+++ Formula/apache-arrow.rb
@@ -1,9 +1,9 @@
 class ApacheArrow < Formula
   desc "Columnar in-memory analytics layer designed to accelerate big data"
   homepage "https://arrow.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=arrow/arrow-1.0.1/apache-arrow-1.0.1.tar.gz"
-  mirror "https://archive.apache.org/dist/arrow/arrow-1.0.1/apache-arrow-1.0.1.tar.gz"
-  sha256 "149ca6aa969ac5742f3b30d1f69a6931a533fd1db8b96712e60bf386a26dc75c"
+  url "https://www.apache.org/dyn/closer.lua?path=arrow/arrow-2.0.0/apache-arrow-2.0.0.tar.gz"
+  mirror "https://archive.apache.org/dist/arrow/arrow-2.0.0/apache-arrow-2.0.0.tar.gz"
+  sha256 "be0342cc847bb340d86aeaef43596a0b6c1dbf1ede9c789a503d939e01c71fbe"
   license "Apache-2.0"
   head "https://github.com/apache/arrow.git"
 
@@ -15,29 +15,34 @@ class ApacheArrow < Formula
     root_url "http://homebrew.stoic.com"
     cellar :any
     rebuild 1
-    sha256 "4dbefb5d52b80cb06be25d6d77cf6ce73f770bfb6c39ae0c6fe1589d5afb05db" => :catalina
+    sha256 "08bd45f687d5c0815f20eef224396c758c62c20a8b66a638a5464462c7a76222" => :catalina
   end
 
-  depends_on "aws-sdk-cpp"
   depends_on "boost" => :build
   depends_on "cmake" => :build
-  depends_on "c-ares"
+  depends_on "llvm@9" => :build
   depends_on "brotli"
   depends_on "glog"
+  depends_on "grpc"
   depends_on "lz4"
   depends_on "numpy"
   depends_on "openssl@1.1"
   depends_on "protobuf"
-  depends_on "python@3.8"
+  depends_on "python@3.9"
   depends_on "rapidjson"
+  depends_on "re2"
   depends_on "snappy"
   depends_on "thrift"
   depends_on "zstd"
 
   def install
     ENV.cxx11
+    # link against system libc++ instead of llvm provided libc++
+    ENV.remove "HOMEBREW_LIBRARY_PATHS", Formula["llvm@9"].opt_lib
     args = %W[
+      -DCMAKE_FIND_PACKAGE_PREFER_CONFIG=TRUE
       -DARROW_FLIGHT=ON
+      -DARROW_GANDIVA=ON
       -DARROW_JEMALLOC=ON
       -DARROW_ORC=ON
       -DARROW_PARQUET=ON
@@ -52,13 +57,10 @@ class ApacheArrow < Formula
       -DARROW_WITH_SNAPPY=ON
       -DARROW_WITH_BROTLI=ON
       -DARROW_INSTALL_NAME_RPATH=OFF
-      -DBUILD_SHARED_LIBS=ON
-      -DgRPC_SOURCE=BUNDLED
-      -DPYTHON_EXECUTABLE=#{Formula["python@3.8"].bin/"python3"}
+      -DPYTHON_EXECUTABLE=#{Formula["python@3.9"].bin/"python3"}
     ]
 
-    mkdir "build"
-    cd "build" do
+    mkdir "build" do
       system "cmake", "../cpp", *std_cmake_args, *args
       system "make"
       system "make", "install"
